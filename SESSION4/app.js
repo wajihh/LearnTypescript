b1850182801b3737d0d242fd9cfab173a8856a40
Wajih Humayun Hashmi <wajihh@yahoo.com>
2024-05-26 13:52:36 +0500

    union literals objects etc

---
+++ SESSION4/app.js
@@ -0,0 +1,16 @@
+//step05b_object_aliased 
+// anonymous
+let teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+let student = {
+    name: "Hira",
+    age: 30
+};
+console.log(student["name"]);
+console.log(student.age);
+let storeManager = {
+    name: "Bilal"
+};
+export {};
